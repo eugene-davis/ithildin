0dd7d91436f8350d2ba8a238ac441d2091af6ea7
Eugene Davis <eugene_davis@industrialdragonfly.com>
2013-11-23 21:41:24 -0600

    Added time tracking.

--- serial/brute_force_serial.cpp
+++ serial/brute_force_serial.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 #include "pass_check.h"
+#include <sys/time.h>
+#include <iomanip>
 
 using namespace std;
+
+/* copied from mpbench */
+#define TIMER_CLEAR     (tv1.tv_sec = tv1.tv_usec = tv2.tv_sec = tv2.tv_usec = 0)
+#define TIMER_START     gettimeofday(&tv1, (struct timezone*)0)
+#define TIMER_ELAPSED   ((tv2.tv_usec-tv1.tv_usec)+((tv2.tv_sec-tv1.tv_sec)*1000000))
+#define TIMER_STOP      gettimeofday(&tv2, (struct timezone*)0)
+struct timeval tv1,tv2;
 
 int enumChars(int cur_pos, unsigned char pass[], int max_length);
 void printPass(int length, unsigned char pass[]);
 
 // Globals
-const unsigned char start_char = 48; // character to start search at
-const unsigned char end_char = 122; // character to end search at
+const unsigned char start_char = 0; // character to start search at
+const unsigned char end_char = 255; // character to end search at
 
 
 int main()
 {
-	int max_length = 8;
+	int max_length = 3;
     unsigned char *pass = new unsigned char[max_length];
    
+	/*
+   Start recording the execution time
+   */
+   TIMER_CLEAR;
+   TIMER_START;
     // Partition on starting letter to make partitions of even size 
     for (unsigned char i = start_char; i < end_char; i++)
     {
@@ -36,6 +50,11 @@ int main()
             }
         }
     }
+	TIMER_STOP;
+
+	cout << "time=" << setprecision(8) <<  TIMER_ELAPSED/1000000.0 
+        << " seconds" << endl;
+	
 	return 0;
 }
 
@@ -68,6 +87,10 @@ int enumChars(int cur_pos, unsigned char pass[], const int max_length)
 
 void printPass(int length, unsigned char pass[])
 {
+	TIMER_STOP;
+	cout << "time=" << setprecision(8) <<  TIMER_ELAPSED/1000000.0 
+        << " seconds" << endl;
+
 	cout << "Success!" << endl;
     cout << "Password was ";
     for (int j = 0; j < length; j++)
